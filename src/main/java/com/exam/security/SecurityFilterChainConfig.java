4c3af614efaf54bc2bd251fa739c45cc1db6fa73
ponion <qkrwndjs9898@gmail.com>
2024-06-20 11:26:11 +0900

    security2 commit

--- src/main/java/com/exam/security/SecurityFilterChainConfig.java
+++ src/main/java/com/exam/security/SecurityFilterChainConfig.java
@@ -3,6 +3,8 @@ package com.exam.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,5 +42,10 @@ public class SecurityFilterChainConfig {
 		     
 		return http.build();
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
 }
